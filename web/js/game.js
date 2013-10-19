be23cd9b92d2c9e0d164ab1d8805f0ae29b395ff
Dominik Jungowski <Dominik@Jungowski.de>
2013-10-20 00:04:29 +0200

    comments for attendes

--- web/js/game.js
+++ web/js/game.js
@@ -3,7 +3,12 @@
     $('.player:last-child table').on('click', function(event) {
         field = $(event.target);
         if (!field.hasClass('used')) {
-            $(event.target).addClass('used');
+            // Hier fehlt noch die Serverinteraktion
+            field.addClass('used');
+
+            // Beispiel: x und y auslesen
+            console.log(field.attr('x'));
+            console.log(field.attr('y'));
         }
     });
 })();
