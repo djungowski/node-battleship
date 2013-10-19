7d384e500afcc72ff0886825d0d9c7b1a4b4d9ae
Dominik Jungowski <Dominik@Jungowski.de>
2013-10-20 01:17:21 +0200

    Objects instead of pure functions

--- web/js/game.js
+++ web/js/game.js
@@ -1,32 +1,43 @@
 (function() {
-    nameForm = $('#enter-player-name');
-    // Spielername als Modal anzeigen
-    nameForm.easyModal({
-        closeOnEscape: false,
-        overlayClose: false,
-        onOpen: function() {
-            nameForm.find('input').focus();
-        }
-    });
-    nameForm.trigger('openModal');
-    nameForm.on('submit', function(event) {
-        event.preventDefault();
+    var Game = function() {
+        this.initNameForm();
+        this.initPlayingField();
+    };
 
-        // Hier fehlt noch die Serverinteraktion
-        nameForm.trigger('closeModal');
-        $('.player.you .player-name').html($('#player-name').val())
-    });
+    Game.prototype.initNameForm = function() {
+        nameForm = $('#enter-player-name');
+        // Spielername als Modal anzeigen
+        nameForm.easyModal({
+            closeOnEscape: false,
+            overlayClose: false,
+            onOpen: function() {
+                nameForm.find('input').focus();
+            }
+        });
+        nameForm.trigger('openModal');
+        nameForm.on('submit', function(event) {
+            event.preventDefault();
 
-    // Klick auf das rechte Spielfeld
-    $('.player.opponent table').on('click', function(event) {
-        field = $(event.target);
-        if (!field.hasClass('used')) {
             // Hier fehlt noch die Serverinteraktion
-            field.addClass('used');
+            nameForm.trigger('closeModal');
+            $('.player.you .player-name').html($('#player-name').val())
+        });
+    };
 
-            // Beispiel: x und y auslesen
-            console.log(field.attr('x'));
-            console.log(field.attr('y'));
-        }
-    });
+    Game.prototype.initPlayingField = function() {
+        // Klick auf das rechte Spielfeld
+        $('.player.opponent table').on('click', function(event) {
+            field = $(event.target);
+            if (!field.hasClass('used')) {
+                // Hier fehlt noch die Serverinteraktion
+                field.addClass('used');
+
+                // Beispiel: x und y auslesen
+                console.log(field.attr('x'));
+                console.log(field.attr('y'));
+            }
+        });
+    };
+
+    window.game = new Game();
 })();
