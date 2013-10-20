4bb02337c12e76d0899c315703c831659411d5f9
Jakob Ketterl <jketterl@chipxonio.de>
2013-10-20 18:14:53 +0200

    add player name events

--- test/battleship/GameInterfaceTest.js
+++ test/battleship/GameInterfaceTest.js
@@ -1,5 +1,6 @@
 var assert = require('assert'),
 	GameInterface = require('../../lib/battleship/GameInterface'),
+	Player = require('../../lib/battleship/Player'),
 	PlayingField = require('../../lib/battleship/PlayingField');
 
 describe('GameInterface', function(){
@@ -16,5 +17,17 @@ describe('GameInterface', function(){
 			var i = new GameInterface();
 			assert.throws(i.shoot);
 		});
+	});
+	
+	it ('should emit an event when the opponent name is changed', function(done){
+		var i = new GameInterface(),
+			op = new Player(1);
+		i.setOpponent(op);
+		i.on('opponentNameChanged', function(name){
+			assert(name === 'some name');
+			done();
+		});
+		
+		op.setName("some name");
 	});
 });
