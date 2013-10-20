6658ff6e56595923081ac23525ef09fd8ccc29ad
Jakob Ketterl <jketterl@chipxonio.de>
2013-10-20 14:40:53 +0200

    basic websocket server integration

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-var express = require("express");
+var http = require("http"),
+	express = require("express"),
+	websocket = require("websocket");
 
 var app = express();
 app.use(express.static(__dirname + '/web'));
 
-app.listen(3000);
+var server = http.createServer(app);
+
+wsServer = new websocket.server({
+	httpServer: server
+});
+
+wsServer.on('request', function(req) {
+	var conn = req.accept();
+});
+
+server.listen(3000);
