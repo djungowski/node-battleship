9371c08cc3bf88a04b18950da42db8da64e02fed
Dominik Jungowski <Dominik@Jungowski.de>
2013-10-22 22:38:27 +0200

    don't show certain elements on start

--- web/js/socket.js
+++ web/js/socket.js
@@ -2,10 +2,7 @@
     var socket = new WebSocket('ws://localhost:3000');
 
     socket.onopen = function(event) {
-        game.setLoading(false);
-        game.showNameForm(true);
-        playingField.renderField();
-        game.initPlayingField();
+
     };
 
     socket.onclose = function(event) {
@@ -19,6 +16,14 @@
         switch(message.status) {
             case 'FULL':
                 $('#loading').html("Der Server ist voll.");
+                break;
+
+            case 'OK':
+                $('.hide-on-start').show();
+                game.setLoading(false);
+                game.showNameForm(true);
+                playingField.renderField();
+                game.initPlayingField();
                 break;
         }
     };
